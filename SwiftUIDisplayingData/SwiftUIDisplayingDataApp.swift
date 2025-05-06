8dbae170c70ee6f9b46588be19284144a78e0876
Kristanto Sean <kristantosean@gmail.com>
2025-05-06 13:36:03 +0700

    Initial Commit

---
+++ SwiftUIDisplayingData/SwiftUIDisplayingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDisplayingDataApp.swift
+//  SwiftUIDisplayingData
+//
+//  Created by Kristanto Sean on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDisplayingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
